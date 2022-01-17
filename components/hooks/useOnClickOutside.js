db5cbd490b982a9ffbd012d3b7354a749e9e51fe
tim-stuff <tim.stuff.dev@gmail.com>
2022-01-17 13:36:51 -0500

    add logo, hide menu when link or button clicked on mobile menu

--- components/hooks/useOnClickOutside.js
+++ components/hooks/useOnClickOutside.js
@@ -1,16 +1,25 @@
 import { useEffect } from 'react';
+import { useWindowSize } from "./useWindowSize";
 
 export const useOnClickOutside = (ref, handler) => {
+    let windowSize = useWindowSize();
     useEffect(() => {
             const listener = event => {
-                if (!ref.current || ref.current.contains(event.target)) {
-                    return;
+                if (windowSize.width > 768) {
+                    if (!ref.current || ref.current.contains(event.target)) {
+                        return;
+                    }
+                } else {
+
+                    if (!ref.current || ref.current.contains(event.target)) {
+                        if (event.target.tagName != "BUTTON" && event.target.tagName != "A") return;
+                    }
                 }
                 handler(event);
             };
-            document.addEventListener('mousedown', listener);
+            document.addEventListener('click', listener);
             return () => {
-                document.removeEventListener('mousedown', listener);
+                document.removeEventListener('click', listener);
             };
         },
         [ref, handler],
--- components/layouts/header/index.js
+++ components/layouts/header/index.js
@@ -1,15 +1,30 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Burger from '../navigation/burger'
 import Menu from '../navigation/menu'
 import { useOnClickOutside } from '../../hooks/useOnClickOutside';
 import styles from './Header.module.css'
+import Link from "next/link";
+
 
 let Header = () => {
     const [open, setOpen] = useState(false);
     const node = useRef();
     useOnClickOutside(node, () => setOpen(false));
+
+    const [hideLogo, setHideLogo] = useState(false);
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            setHideLogo(window.scrollY > 200);
+        });
+    }, []);
+
     return (
         <header ref={node} className={styles.header}>
+            <Link href="/">
+                <a className={` ${styles.logoLink} ${ hideLogo ? styles.hidden : "" }`}>
+                    <img src="/img/ACM_templogo.png" alt="FRC Association Of Churches And Ministries" />
+                </a>
+            </Link>
             <Burger open={open} setOpen={setOpen} />
             <Menu  open={open} setOpen={setOpen} />
         </header>
