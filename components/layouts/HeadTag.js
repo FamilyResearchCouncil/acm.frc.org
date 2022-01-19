2e6bff768edd0e7bc749da118992f0481fa86b78
tim-stuff <tim.stuff.dev@gmail.com>
2022-01-19 11:04:27 -0500

    add Google Analytics page tracking

--- components/layouts/HeadTag.js
+++ components/layouts/HeadTag.js
@@ -1,5 +1,18 @@
-// import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Head from "next/head";
+
+// // log the pageview with their URL
+// export const pageview = (url) => {
+//     window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+//         page_path: url,
+//     })
+// }
+//
+// // log specific events happening.
+// export const event = ({ action, params }) => {
+//     window.gtag('event', action, params)
+// }
+
 
 const HeadTag = (props) => (
     <>
@@ -7,6 +20,22 @@ const HeadTag = (props) => (
             <title>{props.title}</title>
             <meta name="description" content={props.description} />
             <link rel="icon" href="/favicon.ico" />
+            <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+            />
+            <script
+                dangerouslySetInnerHTML={{
+                    __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+                }}
+            />
         </Head>
     </>
 );
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,47 @@
 import React from "react";
 import Layout from "../components/layouts/mainLayout"
-
 import {ThemeProvider} from 'styled-components';
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 import theme from '../components/acmTheme'
-
 import '../styles/globals.css'
+
+
+// log the pageview with their URL
+const pageview = (url) => {
+    console.log("page change");
+    window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+        page_path: url,
+    })
+}
+
+// // log specific events happening.
+// const event = ({ action, params }) => {
+//     console.log("event");
+//     window.gtag('event', action, params)
+// }
+
+
 
 export default function MyApp({ Component, pageProps }) {
     const getLayout = Component.getLayout || ((page) => page);
+    const router = useRouter()
+
+    useEffect(() => {
+        const handleRouteChange = (url) => {
+            pageview(url)
+        }
+        //When the component is mounted, subscribe to router changes
+        //and log those page views
+        router.events.on('routeChangeComplete', handleRouteChange)
+
+        // If the component is unmounted, unsubscribe
+        // from the event with the `off` method
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange)
+        }
+    }, [router.events])
+
     return getLayout(
         <ThemeProvider theme={theme}>
             <Layout>
