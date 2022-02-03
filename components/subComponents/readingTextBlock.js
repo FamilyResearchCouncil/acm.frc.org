171e4113e39a5d30d689e385c9797f7f8716d200
tim-stuff <tim.stuff.dev@gmail.com>
2022-02-03 13:53:59 -0500

    revert text padding changes

--- components/subComponents/readingTextBlock.js
+++ components/subComponents/readingTextBlock.js
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 export const StyledReadingSection = styled.div`
   max-width: ${ ({ theme }) => theme.widths.readingWidth };
   margin: 0 auto;
-  padding: 0;
   text-align: ${props => props.alignment === '' ? 'left' : props.alignment };
   font-size: 1.8rem;
   line-height: 1.6;
