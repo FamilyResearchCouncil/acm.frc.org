23afcc3c2080a68aa2a2e8b34cf1b569c8423a84
tim-stuff <tim.stuff.dev@gmail.com>
2022-02-03 13:53:35 -0500

    update readingText.js

--- components/subComponents/readingTextBlock.js
+++ components/subComponents/readingTextBlock.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const StyledReadingSection = styled.div`
   max-width: ${ ({ theme }) => theme.widths.readingWidth };
   margin: 0 auto;
+  padding: 0;
   text-align: ${props => props.alignment === '' ? 'left' : props.alignment };
   font-size: 1.8rem;
   line-height: 1.6;
